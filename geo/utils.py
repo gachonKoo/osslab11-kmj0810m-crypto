7fbd45972be6d058e26f612a3952f43b0163a19c
kmj0810m-crypto <kmj0810m@gachon.ac.kr>
2025-11-13 23:31:43 +0900

    Refactor geometry functions and add Pythagorean theorem

--- geo/utils.py
+++ geo/utils.py
@@ -1,7 +1,9 @@
 import math
 
-def euclidean(x1, y1, x2, y2):
-  return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def pythagoras(a, b):
+  c = math.sqrt(a**2 + b**2)
+  return c
 
-def circle_area(r):
-  return math.pi * r * r
+def circle(r):
+  area = math.pi * r**2
+  return area
